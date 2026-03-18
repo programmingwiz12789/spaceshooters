b789f354110e9cc26b8b0b15c0a496de56c7e072
Programming Wiz <allisprogramming12789@gmail.com>
2026-03-18 14:55:16 +0700

    Delete SpaceShooters/SpaceShooters/bin/Debug directory

--- SpaceShooters/SpaceShooters/bin/Debug/leaderboard.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE leaderboard;
-
-CREATE TABLE leaderboard (
-    id NUMBER PRIMARY KEY,
-    names VARCHAR(255) NOT NULL,
-    highscore NUMBER NOT NULL,
-    dates VARCHAR(255) NOT NULL
-);
